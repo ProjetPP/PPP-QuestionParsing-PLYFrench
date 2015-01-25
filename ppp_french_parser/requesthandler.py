55c91869da548e3dc2e72c31ab49aa733544026d
Valentin Lorentz <progval@progval.net>
2015-01-25 21:57:20 +0100

    Implement request handler.

--- ppp_french_parser/requesthandler.py
+++ ppp_french_parser/requesthandler.py
@@ -1,12 +1,26 @@
 """Request handler of the module."""
 
+import functools
+from ppp_datamodel import Sentence, TraceItem, Response
 from ppp_libmodule.exceptions import ClientError
+
+from .parser import parse as _parse
+
+@functools.lru_cache(256)
+def parse(s):
+    return _parse(s)
+
 
 class RequestHandler:
     def __init__(self, request):
-        # TODO: Implement this
-        pass
+        self.request = request
 
     def answer(self):
-        # TODO: Implement this
-        pass
+        if self.request.language == 'fr' and \
+                isinstance(self.request.tree, Sentence):
+            tree = parse(self.request.tree.value)
+            meas = {}
+            trace = self.request.trace + [TraceItem('PLYFrenchParser', tree, meas)]
+            return [Response('fr', tree, meas, trace)]
+        else:
+            return []
---
+++ tests/test_requesthandler.py
@@ -0,0 +1,20 @@
+from ppp_datamodel import Missing, Triple, Resource, Sentence
+from ppp_datamodel.communication import Request, TraceItem, Response
+from ppp_libmodule.tests import PPPTestCase
+from ppp_french_parser import app
+
+class TestFrenchParser(PPPTestCase(app)):
+    config_var = 'PPP_FRENCHPARSER'
+    config = ''
+    def testBasics(self):
+        q = Request('1', 'fr', Sentence('Quelle est la date de naissance d’Obama ?'), {}, [])
+        r = self.request(q)
+        self.assertEqual(len(r), 1, r)
+        self.assertEqual(r[0].tree, Triple(Resource('Obama'),
+                Resource('date de naissance'), Missing()))
+
+    def testEnglish(self):
+        q = Request('1', 'en', Sentence('Quelle est la date de naissance d’Obama ?'), {}, [])
+        r = self.request(q)
+        self.assertEqual(len(r), 0, r)
+
