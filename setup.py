9ff8caecacbf7f952f619124bb689ad5bc5f5259
Valentin Lorentz <progval@progval.net>
2015-01-31 11:22:38 +0100

    Bump version number.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ppp_french_parser',
-    version='0.1.2',
+    version='0.1.3',
     description='Parser of the French language for the PPP.',
     url='https://github.com/ProjetPP',
     author='Valentin Lorentz',
